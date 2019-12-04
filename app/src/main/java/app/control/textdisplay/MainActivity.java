93585bba190e853f87bb9ef43ada315a037d3dc1
Start <valliev.work@gmail.com>
2019-12-05 01:52:34 +0600

    add image

--- app/src/main/java/app/control/textdisplay/MainActivity.java
+++ app/src/main/java/app/control/textdisplay/MainActivity.java
@@ -3,6 +3,8 @@ package app.control.textdisplay;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,35 +17,38 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
     Button button1;
+    Button button2;
     EditText edittext1;
     EditText edittext2;
     EditText edittext3;
-    TextView textView;
+    TextView textView1;
+    Image image1;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button1);
-        button1 = findViewById(R.id.button2);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
         edittext1 = findViewById(R.id.editText);
         edittext2 = findViewById(R.id.editText2);
         edittext3 = findViewById(R.id.editText3);
-        textView = findViewById(R.id.textview);
+        textView1 = findViewById(R.id.textview);
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(edittext1.getText());
+                textView1.setText(edittext1.getText());
                 edittext1.setText("");
             }
         });
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String s1 = String.valueOf(edittext2.getText());
@@ -54,15 +59,15 @@ public class MainActivity extends AppCompatActivity {
                     int end = Integer.parseInt(s2);
 
                     if (from >= end) {
-                        textView.setText("Не верный диапазон");
+                        textView1.setText("Не верный диапазон");
                     } else {
                         Random rand = new Random();
                         int dist = end -from;
                         int random = rand.nextInt(dist + 1) + from;
-                        textView.setText(String.valueOf(random));
+                        textView1.setText(String.valueOf(random));
                     }
                 } else {
-                    textView.setText("Поля диапозона не заполнены!");
+                    textView1.setText("Поля диапозона не заполнены!");
                 }
             }
         });
